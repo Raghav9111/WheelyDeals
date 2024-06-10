52320d2dcaf87e837309f9efe4412a908af223aa
HP <HP@LAPTOP-7722HKE3>
2024-06-10 23:58:06 +0530

    Cross Origin Annotation

--- WheelyDeals/src/main/java/com/wheelyDeals/controller/AdminController.java
+++ WheelyDeals/src/main/java/com/wheelyDeals/controller/AdminController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,6 +41,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/admin")
 @RestController
+@CrossOrigin
 public class AdminController extends BaseController {
 	
 	@Autowired
